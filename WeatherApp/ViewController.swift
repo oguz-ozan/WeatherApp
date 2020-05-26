dab2a0c3397760fda689d8f4ffd98c9deada3701
oguz-ozan <oguz.ozan@ceng.deu.edu.tr>
2020-05-26 16:47:48 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Oğuz Özan on 26.05.2020.
+//  Copyright © 2020 Oğuz Özan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
